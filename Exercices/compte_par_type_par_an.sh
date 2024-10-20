ca91fbcf2f435948035d06ff9436141b6c2143d8
Joce <jocelyn.zaruma@proton.me>
2024-10-20 03:57:38 +0200

    ajout du script compte_par_type_par_an.sh

---
+++ Exercices/compte_par_type_par_an.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ENTITY_TYPE=$1
+
+ANN_FOLDER="/Users/jocelyn/Workspace_B/active/sorbonne_nouvelle/prog_project_encadre/04-seance_04/ann"
+
+counter=0
+
+for YEAR_FOLDER in $ANN_FOLDER/*
+do
+    ENTITY_COUNT=$(YEAR_FOLDER="$YEAR_FOLDER" ./compte_par_type.sh "$ENTITY_TYPE")
+
+    YEAR=$(basename "$YEAR_FOLDER")
+    echo -e "Annotations en $YEAR :\n$ENTITY_COUNT"
+
+    counter=$((counter + 1))
+    if [ $counter -ge 3 ]
+    then
+        break
+    fi
+done
