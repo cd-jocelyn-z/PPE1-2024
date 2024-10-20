59ec0f46e9ef1dea979a2db7159f7b087c5c3177
Joce <jocelyn.zaruma@proton.me>
2024-10-20 18:33:42 +0200

    ajout des espaces dans les conditions et petites corrections de syntaxe

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -8,15 +8,15 @@ ANN_FOLDER="/Users/jocelyn/Workspace_B/active/sorbonne_nouvelle/prog_project_enc
 for YEAR_FOLDER in $ANN_FOLDER/*
 do
     YEAR_FOLDER_NAME=$(basename "$YEAR_FOLDER")
-    if [$YEAR=="$YEAR_FOLDER_NAME"] || [$YEAR=="*"]
+    if [ "$YEAR" == "$YEAR_FOLDER_NAME" ] || [ "$YEAR" == "*" ]
     then
-        for MONTH_FOLDER in YEAR_FOLDER/*
+        for MONTH_FOLDER in $YEAR_FOLDER/*
         do
             MONTH_FOLDER_NAME=$(basename "$MONTH_FOLDER")
-            if [$MONTH=="$MONTH_FOLDER_NAME"] || [$MONTH=="*"]
+            if [ "$MONTH" == "$MONTH_FOLDER_NAME" ] || [ "$MONTH" == "*" ]
             then
                 ANN_FILES="$MONTH_FOLDER/*.ann"
-                LOCATION_RANKINGS=$(cat $ANN_FILES | grep "Location" | cut -f 3 | sort | uniq -c | sort | head -n "$TOP_N_LOCATIONS")
+                LOCATION_RANKINGS=$(cat $ANN_FILES | grep "Location" | cut -f 3 | sort | uniq -c | sort | tail -n "$TOP_N_LOCATIONS")
                 echo "$LOCATION_RANKINGS"
             fi
         done
