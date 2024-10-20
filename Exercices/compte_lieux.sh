a7ea4b8f1d9d0522c8849aafb79bd7d2ee7e9ea2
Joce <jocelyn.zaruma@proton.me>
2024-10-20 21:08:11 +0200

    remplacement des boucles par des conditions pour gérer les cas

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -5,20 +5,20 @@ TOP_N_LOCATIONS=$3
 
 ANN_FOLDER="/Users/jocelyn/Workspace_B/active/sorbonne_nouvelle/prog_project_encadre/04-seance_04/ann"
 
-for YEAR_FOLDER in $ANN_FOLDER/*
-do
-    YEAR_FOLDER_NAME=$(basename "$YEAR_FOLDER")
-    if [ "$YEAR" == "$YEAR_FOLDER_NAME" ] || [ "$YEAR" == "*" ]
+if [ "$YEAR" != "*" ]
+then
+    if [ "$MONTH" != "*" ]
     then
-        for MONTH_FOLDER in $YEAR_FOLDER/*
-        do
-            MONTH_FOLDER_NAME=$(basename "$MONTH_FOLDER")
-            if [ "$MONTH" == "$MONTH_FOLDER_NAME" ] || [ "$MONTH" == "*" ]
-            then
-                ANN_FILES="$MONTH_FOLDER/*.ann"
-                LOCATION_RANKINGS=$(cat $ANN_FILES | grep "Location" | cut -f 3 | sort | uniq -c | sort | tail -n "$TOP_N_LOCATIONS")
-                echo "$LOCATION_RANKINGS"
-            fi
-        done
+	ANN_FILES="$ANN_FOLDER/$YEAR/$MONTH/*.ann"
+    else
+	ANN_FILES="$ANN_FOLDER/$YEAR/*/*"
     fi
-done
+else
+    if [ "$MONTH" != "*" ]
+    then
+	ANN_FILES="$ANN_FOLDER/*/$MONTH/*.ann"
+    fi
+fi
+
+LOCATION_RANKINGS=$(cat $ANN_FILES | grep "Location" | cut -f 3 | sort | uniq -c | sort | tail -n "$TOP_N_LOCATIONS")
+echo "$LOCATION_RANKINGS"
