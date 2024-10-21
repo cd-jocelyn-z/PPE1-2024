8d5ba71d8c44133c2aa779c63c56d253b697c9d6
Joce <jocelyn.zaruma@proton.me>
2024-10-21 22:07:51 +0200

    ajout de validation des arguments

--- Exercices/compte_par_type.sh
+++ Exercices/compte_par_type.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ENTITY_TYPE=$1
+if [ $# -ne 1 ]
+then
+    echo "Ce programme demande un argument: un type d'entité (ex: Location, Person, Organization etc.)."
+    exit 1
+fi
 
 ENTITY_COUNT=$(cat $YEAR_FOLDER/*/*.ann | grep "$ENTITY_TYPE" | wc -l)
 echo "$ENTITY_COUNT"
